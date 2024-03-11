da56b5e6a51609034d558091f9cfd0d1e5850058
hiu lam tse <hiulam1@hotmail.com>
2024-03-11 15:36:37 +0100

    user model

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'dotenv/config';
 import mongoose from 'mongoose';
+import cors from 'cors';
+import morgan from 'morgan'; // Add import for 'morgan' module
 
 const app = express();
 const PORT = 3000;
@@ -9,6 +11,9 @@ const PORT = 3000;
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript with Express!');
 });
+
+app.use(cors({origin:'http://localhost:3000'})); // to change before deployment
+app.use(morgan('dev'));
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
